261d64ca6d94c858fd07e9048140ccb0c8653f42
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-20 12:57:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Reflection in c#...");
+            Type T = typeof(int);
+            Console.WriteLine("Type of Name:{0}", T.Name);
+            Console.WriteLine("Full name of the type defined :\t{0}", T.FullName);
+            Console.WriteLine("Type of Assembly in which it is defined :\t {0} ", T.Assembly);
+            Console.WriteLine("Information about base Type:\t{0}", T.BaseType);
+
+            Dynamic_Object MyObj1 = new Dynamic_Object();
+            MyObj1.First_Method();
+            MyObj1.Second_Method();
+
+            Dynamic_Object MyObj2 = new Dynamic_Object();
+            //MyObj2.First_Method("msg from Dymanic object");
+            MyObj2.Second_Method();
+            MyObj2.First_Method();
+        }
+    }
+}
---
+++ ReflectionClass1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Reflection
+{
+
+    class Dynamic_Object
+    {
+        public Dynamic_Object() { }
+        public Dynamic_Object(String msg) { }
+
+        public void First_Method() {
+            Console.WriteLine("Inside First Method");
+        }
+        
+        public void Second_Method() {
+            Console.WriteLine("Inside Second Method");
+        }
+    }
+}
